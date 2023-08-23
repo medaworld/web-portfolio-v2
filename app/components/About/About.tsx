e9d068f1e74cc2601c791809e55d97b302b323cc
MEDA <118144136+medaworld@users.noreply.github.com>
2023-08-22 23:37:15 -0700

    Add transition to about me section

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -2,10 +2,25 @@ import { robotoSlab } from '@/app/fonts';
 import AboutContent from './AboutContent';
 import AboutTechStack from './AboutTechStack';
 import { SectionContainer, SectionTitle } from '../common/CommonStyles';
+import { useEffect, useRef } from 'react';
+import usePrefersReducedMotion from '@/app/hooks/usePrefersReducedMotion';
+import sr, { srConfig } from '@/app/utils/scrollUtils';
 
 export default function About() {
+  const revealContainer = useRef<HTMLDivElement>(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion || !sr || !revealContainer.current) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <SectionContainer id={'about'}>
+    <SectionContainer id={'about'} ref={revealContainer}>
       <SectionTitle className={robotoSlab.className}>About</SectionTitle>
       <AboutContent />
       <AboutTechStack />
